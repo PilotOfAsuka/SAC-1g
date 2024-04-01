bd61994390774fce120ad0b5948d6ce9b3fd6dc0
PilotOfAsuka <150352161+PilotOfAsuka@users.noreply.github.com>
2024-04-01 09:43:57 +0500

    Update admin_panael.py
    Переменную со списком администраторов, перенес в .env

--- modules/admin_panael.py
+++ modules/admin_panael.py
@@ -1,19 +1,17 @@
+import os
+
 from aiogram.types import Message
 from functools import wraps
 
 
-admins = [
-    5848061277,  # Илья
-    5611067209,  # Макс
-
-]
+admins = os.getenv("admins")
 
 
 # Функция-декоратор для проверки администраторских прав
 def check_admins(func):
     @wraps(func)
     async def wrapper(mes: Message, *args, **kwargs):
-        if mes.from_user.id not in admins:
+        if str(mes.from_user.id) not in admins:
             await mes.answer("У вас нет прав на выполнение этой команды. Обратитесь к разработчику!")
             return
         return await func(mes, *args, **kwargs)
