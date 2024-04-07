16a98445043b5a3ff2d2877a6ccae70bec99401e
PilotOfAsuka <150352161+PilotOfAsuka@users.noreply.github.com>
2024-04-07 09:57:45 +0500

    Update dth22.py

--- modules/dth22.py
+++ modules/dth22.py
@@ -1,13 +1,18 @@
 #Libraries
-import Adafruit_DHT as dht
+import board
+import Adafruit_DHT as adafruit
 from time import sleep
 
-#Set DATA pin
-DHT = 4
+#Set sensor
+DHT = adafruit.DHT22(board.D4)
 
 def get_dht_data():
-    h, t = dht.read_retry(dht.DHT22, DHT)
-    return round(t, 1), round(h, 1)
+    # Print the values to the serial port
+    temperature_c = DHT.temperature
+    temperature_f = temperature_c * (9 / 5) + 32
+    humidity = DHT.humidity
+    print("Temp={0:0.1f}ºC, Temp={1:0.1f}ºF, Humidity={2:0.1f}%".format(temperature_c, temperature_f, humidity))
+    return temperature_c, humidity
 
 
 
