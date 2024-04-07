6661bf9772b91c177a81a700fd6edbf2290180e8
PilotOfAsuka <150352161+PilotOfAsuka@users.noreply.github.com>
2024-04-07 11:07:25 +0500

    Update dth22.py

--- modules/dth22.py
+++ modules/dth22.py
@@ -4,14 +4,16 @@ import Adafruit_DHT as dht
 
 
 # Set DATA pin
-DHT = 7
+DHT = 23
 
 
 def get_dht_data():
-    GPIO.cleanup()
-    h, t = dht.read_retry(dht.DHT22, DHT)
-    return round(t, 1), round(h, 1)
-
+    try:
+        GPIO.cleanup()
+        h, t = dht.read_retry(dht.DHT22, DHT)
+        return round(t, 1), round(h, 1)
+    except TypeError:
+        return 0, 0
 
 
 
