0b6e53c0ee01b796b803f98a1c298c85ac698fe7
PilotOfAsuka <150352161+PilotOfAsuka@users.noreply.github.com>
2024-04-07 11:00:07 +0500

    test

--- modules/dth22.py
+++ modules/dth22.py
@@ -1,11 +1,14 @@
 #Libraries
+import RPi.GPIO as GPIO
 import Adafruit_DHT as dht
-from time import sleep
 
-#Set DATA pin
+
+# Set DATA pin
 DHT = 4
 
+
 def get_dht_data():
+    GPIO.cleanup()
     h, t = dht.read_retry(dht.DHT22, DHT)
     return round(t, 1), round(h, 1)
 
