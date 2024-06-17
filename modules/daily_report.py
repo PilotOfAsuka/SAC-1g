1f0b7fc48b702783b51af3910e3997794d4c48ab
PilotOfAsuka <150352161+PilotOfAsuka@users.noreply.github.com>
2024-06-17 21:41:09 +0500

    Update daily_report.py

--- modules/daily_report.py
+++ modules/daily_report.py
@@ -16,7 +16,7 @@ async def send_message_func(chat_id):
     t, h, voltage = get_mi_sensor_data(Box_device_address)
     t2, h2, voltage2 = get_mi_sensor_data(Room_device_address)
     mid_t = (t+t2)/2
-    daily_data.append((datetime.now(), t, h, t2, h2))  # Сохранение данных с меткой времени
+    daily_data.append((datetime.now(), t, t2))  # Сохранение данных с меткой времени
     await misc.bot.send_message(chat_id=chat_id,
                            text=f"Температура: {t}°C, Влажность: {h}%\nТемпература2: {t2}°C, Влажность2: {h2}%\nСредняя темп.: {mid_t}")
 
@@ -24,46 +24,47 @@ async def send_message_func(chat_id):
 def create_plot():
     times = [data[0] for data in daily_data]
     temps1 = [data[1] for data in daily_data]
-    hums1 = [data[2] for data in daily_data]
-    temps2 = [data[3] for data in daily_data]
-    hums2 = [data[4] for data in daily_data]
+    temps2 = [data[2] for data in daily_data]
 
+    # График для Temperature1
     plt.figure(figsize=(10, 6))
-
-    plt.subplot(2, 1, 1)
     plt.plot(times, temps1, label='Temperature1 (°C)', color='r')
-    plt.plot(times, temps2, label='Temperature2 (°C)', color='b')
     plt.ylabel('Temperature (°C)')
-    plt.legend()
-    plt.title('Temperature and Humidity Over Time')
-
-    plt.subplot(2, 1, 2)
-    plt.plot(times, hums1, label='Humidity1 (%)', color='r')
-    plt.plot(times, hums2, label='Humidity2 (%)', color='b')
-    plt.ylabel('Humidity (%)')
     plt.xlabel('Time')
     plt.legend()
-
+    plt.title('Temperature1 Over Time')
     plt.tight_layout()
-
-    plot_filename = os.path.join(REPORTS_DIR, f"daily_plot_{datetime.now().strftime('%Y%m%d')}.png")
-    plt.savefig(plot_filename)
+    plot_filename1 = os.path.join(REPORTS_DIR, f"temperature1_plot_{datetime.now().strftime('%Y%m%d')}.png")
+    plt.savefig(plot_filename1)
     plt.close()
 
-    return plot_filename
+    # График для Temperature2
+    plt.figure(figsize=(10, 6))
+    plt.plot(times, temps2, label='Temperature2 (°C)', color='b')
+    plt.ylabel('Temperature (°C)')
+    plt.xlabel('Time')
+    plt.legend()
+    plt.title('Temperature2 Over Time')
+    plt.tight_layout()
+    plot_filename2 = os.path.join(REPORTS_DIR, f"temperature2_plot_{datetime.now().strftime('%Y%m%d')}.png")
+    plt.savefig(plot_filename2)
+    plt.close()
+
+    return plot_filename1, plot_filename2
 
 
 async def generate_daily_report(chat_id):
     # Создание графика
-    plot_filename = create_plot()
+    plot_filename1, plot_filename2 = create_plot()
 
     # Отправка ежедневного графика
-    await misc.bot.send_photo(chat_id=chat_id, photo=types.FSInputFile(plot_filename))
-
+    await misc.bot.send_photo(chat_id=chat_id, photo=types.FSInputFile(plot_filename1))
+    await misc.bot.send_photo(chat_id=chat_id, photo=types.FSInputFile(plot_filename2))
     # Сброс данных
     daily_data.clear()
 
-    os.remove(plot_filename)
+    os.remove(plot_filename1)
+    os.remove(plot_filename2)
 
 
 async def daily_report_task():
